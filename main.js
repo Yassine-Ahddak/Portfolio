560d34ccdf30024f3f1d4663416a6395915dcbb0
Yassine Ahddak <72125501+yas93@users.noreply.github.com>
2021-11-05 22:38:30 +0100

    push2

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-/*const divIcon = */
-
-
 
 $(document).ready(function () {
-    
+
+    //Navbar sticky
     $(window).scroll(function(){
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
@@ -18,10 +16,47 @@ $(document).ready(function () {
         $('.menu-btn i').toggleClass("active");
     });
 
-    $('.img_gauche').click(function () {
+    /*$('.img_gauche').click(function () {
         document.getElementById('img_droite').style.display=none;
         document.getElementById('clic_img_droite').style.display=inline;
 
-    });
+    });*/
+
+    //const plus = document.querySelectorAll(".dut_formation");
+    //const zonePlus = document.querySelectorAll(".zone_plus");
+
+    const plus = document.getElementsByClassName("dut_formation");
+    const zonePlus = document.getElementsByClassName("zone_plus");
+    const imgLess = document.getElementsByClassName("clic_img_droite");
+    const imgDroite = document.getElementsByClassName("img_droite");
+
+    plus[0].addEventListener("click", () =>{
+        zonePlus[0].style.display = "block";
+        imgDroite[0].style.display="none";
+        imgLess[0].style.display="block";
+    })
+
+    plus[1].addEventListener("click", () =>{
+        zonePlus[1].style.display = "block";
+        imgDroite[1].style.display="none";
+        imgLess[1].style.display="block";
+    })
+
+    if((zonePlus[0].style.display = "block") === true){
+        plus[0].addEventListener("click", () =>{
+            zonePlus[0].style.display = "none";
+            imgDroite[0].style.display="block";
+            imgLess[0].style.display="none";
+        })
+    }
+
 });
+
+
+
+
+
+
+
+
 
